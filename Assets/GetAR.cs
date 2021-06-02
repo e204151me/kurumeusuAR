ce691cebfd561b2f11ca488a11b8da17de511f89
forestfairy-495 <eishu0821@gmail.com>
2021-06-02 19:04:46 +0900

    ベースプロジェクト

---
+++ Assets/GetAR.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GetAR : MonoBehaviour
+{
+    public static bool IsActive;
+    MeshRenderer mesh;
+    void Start() => mesh = GetComponent<MeshRenderer>();
+    void FixedUpdate() => IsActive = mesh.enabled;
+}
+
+
